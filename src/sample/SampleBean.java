f7d5772751423c1fa2882f2aae9592f0a6740d0a
Nolan Chappuis <nolan.chappuis@epfl.ch>
2021-01-16 00:20:03 +0100

    Added files

---
+++ src/sample/SampleBean.java
@@ -0,0 +1,152 @@
+package sample;
+
+public class SampleBean {
+
+	private String storingLocation;
+	private String sampleSequencer;
+	private String sampleStocker;
+	private String sampleUploader;
+	private String sampleIdentifier;
+	private String sampleCollector;
+	private String sampleExtractor;
+	private String sequencingMethodName;
+	private String amplificationMethodName;
+	private String extractionMethodName;
+	private String specimenStoringID;
+	private String sequencingTimestamp;
+	private String amplificationTimestamp;
+	private String extractionTimestamp;
+	private String primerCodes;
+	private String readDirection;
+	private String sequenceLength;
+	private String marker;
+	private String primerDescription;
+	private String sequenceData;
+	private String baseCallingFile;
+	public String getStoringLocation() {
+		return storingLocation;
+	}
+	public void setStoringLocation(String storingLocation) {
+		this.storingLocation = storingLocation;
+	}
+	public String getSampleSequencer() {
+		return sampleSequencer;
+	}
+	public void setSampleSequencer(String sampleSequencer) {
+		this.sampleSequencer = sampleSequencer;
+	}
+	public String getSampleStocker() {
+		return sampleStocker;
+	}
+	public void setSampleStocker(String sampleStocker) {
+		this.sampleStocker = sampleStocker;
+	}
+	public String getSampleUploader() {
+		return sampleUploader;
+	}
+	public void setSampleUploader(String sampleUploader) {
+		this.sampleUploader = sampleUploader;
+	}
+	public String getSampleIdentifier() {
+		return sampleIdentifier;
+	}
+	public void setSampleIdentifier(String sampleIdentifier) {
+		this.sampleIdentifier = sampleIdentifier;
+	}
+	public String getSampleCollector() {
+		return sampleCollector;
+	}
+	public void setSampleCollector(String sampleCollector) {
+		this.sampleCollector = sampleCollector;
+	}
+	public String getSampleExtractor() {
+		return sampleExtractor;
+	}
+	public void setSampleExtractor(String sampleExtractor) {
+		this.sampleExtractor = sampleExtractor;
+	}
+	public String getSequencingMethodName() {
+		return sequencingMethodName;
+	}
+	public void setSequencingMethodName(String sequencingMethodName) {
+		this.sequencingMethodName = sequencingMethodName;
+	}
+	public String getAmplificationMethodName() {
+		return amplificationMethodName;
+	}
+	public void setAmplificationMethodName(String amplificationMethodName) {
+		this.amplificationMethodName = amplificationMethodName;
+	}
+	public String getExtractionMethodName() {
+		return extractionMethodName;
+	}
+	public void setExtractionMethodName(String extractionMethodName) {
+		this.extractionMethodName = extractionMethodName;
+	}
+	public String getSpecimenStoringID() {
+		return specimenStoringID;
+	}
+	public void setSpecimenStoringID(String specimenStoringID) {
+		this.specimenStoringID = specimenStoringID;
+	}
+	public String getSequencingTimestamp() {
+		return sequencingTimestamp;
+	}
+	public void setSequencingTimestamp(String sequencingTimestamp) {
+		this.sequencingTimestamp = sequencingTimestamp;
+	}
+	public String getAmplificationTimestamp() {
+		return amplificationTimestamp;
+	}
+	public void setAmplificationTimestamp(String amplificationTimestamp) {
+		this.amplificationTimestamp = amplificationTimestamp;
+	}
+	public String getExtractionTimestamp() {
+		return extractionTimestamp;
+	}
+	public void setExtractionTimestamp(String extractionTimestamp) {
+		this.extractionTimestamp = extractionTimestamp;
+	}
+	public String getPrimerCodes() {
+		return primerCodes;
+	}
+	public void setPrimerCodes(String primerCodes) {
+		this.primerCodes = primerCodes;
+	}
+	public String getReadDirection() {
+		return readDirection;
+	}
+	public void setReadDirection(String readDirection) {
+		this.readDirection = readDirection;
+	}
+	public String getSequenceLength() {
+		return sequenceLength;
+	}
+	public void setSequenceLength(String sequenceLength) {
+		this.sequenceLength = sequenceLength;
+	}
+	public String getMarker() {
+		return marker;
+	}
+	public void setMarker(String marker) {
+		this.marker = marker;
+	}
+	public String getPrimerDescription() {
+		return primerDescription;
+	}
+	public void setPrimerDescription(String primerDescription) {
+		this.primerDescription = primerDescription;
+	}
+	public String getSequenceData() {
+		return sequenceData;
+	}
+	public void setSequenceData(String sequenceData) {
+		this.sequenceData = sequenceData;
+	}
+	public String getBaseCallingFile() {
+		return baseCallingFile;
+	}
+	public void setBaseCallingFile(String baseCallingFile) {
+		this.baseCallingFile = baseCallingFile;
+	}
+}
